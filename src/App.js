2a787c8b0a3fabb600dca7c7d764188fc2fcef74
renatakov <renatakov1010@gmail.com>
2023-12-24 14:26:11 +0100

    1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/Header/Header";
+import s from "./App.css"
+import Main from "./components/Main/Main";
+const App = () => {
+  return (
+    <>
+    <Header/>
+    <Main/>
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,11 @@
+import s from "./Main.module.css"
+
+const Main = () => {
+    return(
+        <main>
+
+        </main>
+    )
+}
+
+export default Main;
