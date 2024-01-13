9ea76343d762e1d46fbee7a2bf919ab41d053f69
renatakov <renatakov1010@gmail.com>
2024-01-13 10:34:23 +0100

    1

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,19 +3,38 @@ import axios from "axios";
 import { useState, useEffect, useRef } from "react";
 import Country from "../Country/Country"
 import SearchList from "../SearchList/SearchList";
+import Select from "react-dropdown-select"
 
 const Main = () => {
     const [searchList, setSearchList] = useState([])
     const [countries, setCountries] = useState([])
+    const options = [
+        {
+            value: 1,
+            label: "Africa"
+        },
+        {
+            value: 2,
+            label: "America"
+        },
+        {
+            value: 3,
+            label: "Asia"
+        },
+        {
+            value: 4,
+            label: "Europe"
+        },
+        {
+            value: 5,
+            label: "Oceania"
+        }
+    ];
     const inputSearchRef = useRef(null)
     function selectFunc(e) {
-        if(e.target.options.selectedIndex === 0){
-            axios.get("https://restcountries.com/v3.1/all")
+            axios.get(`https://restcountries.com/v3.1/region/${e}`)
                 .then((data) => setCountries(data.data))
-        } else{
-            axios.get(`https://restcountries.com/v3.1/region/${e.target[e.target.options.selectedIndex].innerText.toLowerCase()}`)
-            .then((data)=>setCountries(data.data))
-        }
+        
     }
     function handleInput() {
         if (inputSearchRef.current.value === '') {
@@ -53,16 +72,8 @@ const Main = () => {
                     <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" /></svg>
                     <input onInput={handleInput} ref={inputSearchRef} className={s.inputCountry} type="text" placeholder="Search for a country..." />
                 </div>
-                <div className={s.filter}>
-                    <select onChange={selectFunc} name="regions" className={s.filterSelect}>
-                        <option className={s.option}>Filter by Region:</option>
-                        <option className={s.option}>Africa</option>
-                        <option className={s.option}>America</option>
-                        <option className={s.option}>Asia</option>
-                        <option className={s.option}>Europe</option>
-                        <option className={s.option}>Oceania</option>
-                    </select>
-                </div>
+
+                <Select searchable={false} clearable={false} options={options}  placeholder="Filter By Region" onChange={(e)=>selectFunc(e[0].label)} />
             </div>
             <div className={s.countries}>
                 {inputSearchRef.current && inputSearchRef.current.value == "" ? Collection() : <SearchList list={searchList} />}
